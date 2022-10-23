710674c03123de6e76a95b3e7333e727b849331d
hp <sahalnurdin@gmail.com>
2022-10-23 20:42:05 +0700

    add some files again

--- Pertemuan 2 Lepkom/pert2act1.go
+++ Pertemuan 2 Lepkom/pert2act1.go
@@ -12,14 +12,27 @@
 
 // }
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var nama1 string = "Hello"
+// 	var nama2 string = " Saya Belajar"
+// 	var nama3 string = " Golang"
+
+// 	fmt.Printf("%s%s%s", nama1, nama2, nama3)
+// }
+
 package main
 
 import "fmt"
 
 func main() {
-	var nama1 string = "Hello"
-	var nama2 string = " Saya Belajar"
-	var nama3 string = " Golang"
+	var f_name string = "Muhammad"
+	m_name := "Sahal"
+	l_name := "Nurdin"
+	age := 19
 
-	fmt.Printf("%s%s%s", nama1, nama2, nama3)
+	fmt.Printf("My name is %s %s %s, I am %d old\n", f_name, m_name, l_name, age)
 }
