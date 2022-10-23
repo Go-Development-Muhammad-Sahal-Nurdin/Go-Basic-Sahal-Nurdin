5f72c317fad7704468169f608ff40b9272302a7e
hp <sahalnurdin@gmail.com>
2022-10-23 20:54:50 +0700

    add some files again

--- Pertemuan 2 Lepkom/pert2act2.go
+++ Pertemuan 2 Lepkom/pert2act2.go
@@ -1,14 +1,29 @@
+// package main
+
+// import "fmt"
+
+// const pi = 22 / 7
+
+// var jari float64
+
+// func main() {
+// 	fmt.Printf("Masukkan jari-jari lingkaran = ")
+// 	fmt.Scan(&jari)
+// 	luas := pi * (jari * jari)
+// 	fmt.Println("Luas Lingkaran = ", luas)
+// }
+
 package main
 
 import "fmt"
 
-const pi = 3.14
-var jari int
+const pi = 22 / 7
 
+var jari int
 
 func main() {
 	fmt.Printf("Masukkan jari-jari lingkaran = ")
 	fmt.Scan(&jari)
 	luas := pi * (jari * jari)
-	fmt.Println("Luas Lingkaran = "luas)
+	fmt.Println("Luas Lingkaran = ", luas)
 }
