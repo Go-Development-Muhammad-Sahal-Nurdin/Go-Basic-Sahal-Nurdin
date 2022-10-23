e8519392d5399a65dcb1cf12581476741e64558a
hp <sahalnurdin@gmail.com>
2022-10-23 20:36:58 +0700

    add some features

---
+++ Pertemuan 2 Lepkom/pert2act1.go
@@ -0,0 +1,25 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var nama1 string = "Hello"
+// 	var nama2 string = " Saya Belajar"
+// 	var nama3 string = " Golang"
+// 	fmt.Print(nama1)
+// 	fmt.Print(nama2)
+// 	fmt.Print(nama3)
+
+// }
+
+package main
+
+import "fmt"
+
+func main() {
+	var nama1 string = "Hello"
+	var nama2 string = " Saya Belajar"
+	var nama3 string = " Golang"
+
+	fmt.Printf("%s%s%s", nama1, nama2, nama3)
+}
