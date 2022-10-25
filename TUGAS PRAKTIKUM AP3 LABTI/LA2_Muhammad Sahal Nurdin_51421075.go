b9b94d4ba48257a052e54fab1fdb2a1c6e462bef
hp <sahalnurdin@gmail.com>
2022-10-25 14:02:03 +0700

    add changes

--- TUGAS PRAKTIKUM AP3 LABTI/LA2_Muhammad Sahal Nurdin_51421075.go
+++ TUGAS PRAKTIKUM AP3 LABTI/LA2_Muhammad Sahal Nurdin_51421075.go
@@ -11,13 +11,15 @@ func sum(arr []int) int {
 }
 func main() {
 
-	tinggiBadan := []int{169, 165, 169, 160, 170}
-	first := tinggiBadan[0]
-	var p *int
-	p = &first
+	tinggiBadan := []int{169, 165, 169, 160, 170, 170}
+	//first := tinggiBadan[0]
+
 	fmt.Println("Berikut ini adalah list dari tinggi badan teman saya: ", tinggiBadan)
 	res := sum(tinggiBadan)
-	fmt.Println("Rata-rata tinggi badan adalah: ", res/len(tinggiBadan))
-	fmt.Println(first)
+	var rataRataTinggi int = res / len(tinggiBadan)
+	fmt.Println("Rata-rata tinggi badan adalah: ", rataRataTinggi)
+	var p *int
+	p = &rataRataTinggi
+	fmt.Println(rataRataTinggi)
 	fmt.Println(p)
 }
