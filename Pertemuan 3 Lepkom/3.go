888d8b8b822c3efbda3f044be989b604f92bdf17
hp <sahalnurdin@gmail.com>
2022-10-30 10:09:46 +0700

    add files

--- Pertemuan 3 Lepkom/3.go
+++ Pertemuan 3 Lepkom/3.go
@@ -28,18 +28,26 @@ package main
 import "fmt"
 
 func main() {
-	var i, inputNilai int
+	var i int = 1
+	var j int
 
-	for i = 0; i < 10; i++ {
-
+	for {
 		fmt.Print("Input Nilai I: ")
-		fmt.Scan(&inputNilai)
-		if inputNilai%2 == 0 {
-			fmt.Println(inputNilai, "Adalah Angka Genap")
-		} else if inputNilai%2 != 0 {
-			fmt.Println(inputNilai, "Adalah Angka Ganjil")
+		fmt.Scan(&j)
+		i++
+		if j%2 == 0 {
+			fmt.Println(j, "Adalah Angka Genap")
+		} else if j%2 != 0 {
+			fmt.Println(j, "Adalah Angka Ganjil")
+		} else {
+			fmt.Print("Something Wrong!")
 		}
+		j = j + 1
+		if i > 10 {
+			fmt.Print("Pertanyaan selesai, Karena angka yang dapat di-input sudah melebihi dari 10. Terima Kasih")
+			break
 
+		}
 	}
 
 }
