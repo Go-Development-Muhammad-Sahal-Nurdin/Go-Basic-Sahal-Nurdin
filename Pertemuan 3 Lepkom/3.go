1d76ad2b4852ddb0ad34c1816af9aecd36b0bfc8
hp <sahalnurdin@gmail.com>
2022-10-30 10:02:54 +0700

    add files

--- Pertemuan 3 Lepkom/3.go
+++ Pertemuan 3 Lepkom/3.go
@@ -1,13 +1,45 @@
+// package main
+
+//  Muhammad Sahal Nurdin 51421075
+
+// import "fmt"
+
+// func main() {
+// 	var i, inputNilai int
+
+// 	for i = 0; i < 10; i++ {
+
+// 		fmt.Print("Input Nilai I: ")
+// 		fmt.Scan(&inputNilai)
+// 		if inputNilai%2 == 0 {
+// 			fmt.Println(inputNilai, "Adalah Angka Genap")
+// 		} else if inputNilai%2 != 0 {
+// 			fmt.Println(inputNilai, "Adalah Angka Ganjil")
+// 		}
+
+// 	}
+
+// }
+
 package main
+
+// Muhammad Sahal Nurdin 51421075
 
 import "fmt"
 
 func main() {
-	var i int
+	var i, inputNilai int
 
 	for i = 0; i < 10; i++ {
 
+		fmt.Print("Input Nilai I: ")
+		fmt.Scan(&inputNilai)
+		if inputNilai%2 == 0 {
+			fmt.Println(inputNilai, "Adalah Angka Genap")
+		} else if inputNilai%2 != 0 {
+			fmt.Println(inputNilai, "Adalah Angka Ganjil")
+		}
+
 	}
 
-	fmt.Print()
 }
