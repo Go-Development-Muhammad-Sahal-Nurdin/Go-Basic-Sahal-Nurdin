f287c61f6410fe938473786f3ae10a74d07602f0
hp <sahalnurdin@gmail.com>
2022-11-08 19:28:49 +0700

    add changes

--- Pertemuan 4 Lepkom/gaji.go
+++ Pertemuan 4 Lepkom/gaji.go
@@ -6,5 +6,16 @@ func main() {
 	var gajiSekarang, ekspektasiGaji int
 
 	fmt.Print("Masukkan gaji anda: ")
+	fmt.Scan(&gajiSekarang)
 
+	fmt.Print("Masukkan gaji anda inginkan : ")
+	fmt.Scan(&ekspektasiGaji)
+
+	naikanGaji(gajiSekarang, ekspektasiGaji)
+
+	fmt.Printf("\nGaji anda sekarang %d\n", gajiSekarang)
+}
+
+func naikanGaji(gajiAwal int, gajiaAkhir int) {
+	gajiAwal = gajiaAkhir
 }
