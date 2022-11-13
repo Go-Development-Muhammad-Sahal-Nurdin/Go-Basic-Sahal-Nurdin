5703b107b288b830e8691819e38621b619a523b6
hp <sahalnurdin@gmail.com>
2022-11-13 19:27:31 +0700

    changes

--- Materi Pak Khannedy/variable.go
+++ Materi Pak Khannedy/variable.go
@@ -8,4 +8,24 @@ func main() {
 	name = "Muhammad Sahal Nurdin"
 	fmt.Println(name)
 
+	name = "sahal"
+	fmt.Println(name)
+
+	var friendName = "Budi"
+	fmt.Println(friendName)
+
+	var age int8 = 19
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Muhammad Sahal"
+		lastName  = "Nurdin"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
 }
