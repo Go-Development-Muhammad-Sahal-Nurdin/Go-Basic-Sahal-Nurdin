536fbb1bdc218c96bd7d4fdc2834fe5dbf3c1dc6
hp <sahalnurdin@gmail.com>
2022-11-13 19:41:51 +0700

    channges

--- Materi Pak Khannedy/constant.go
+++ Materi Pak Khannedy/constant.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	const firstName string = "Muhammad Sahal Nurdin"
-	const lastName = "Nurdin"
-	const umur = 18
-
-	fmt.Println(firstName)
+	const (
+		firstName string = "Muhammad Sahal Nurdin"
+		lastName         = "Nurdin"
+		umur             = 18
+	)
+	// fmt.Println(firstName)
 	fmt.Println(lastName)
+	fmt.Println(umur)
+
 }
