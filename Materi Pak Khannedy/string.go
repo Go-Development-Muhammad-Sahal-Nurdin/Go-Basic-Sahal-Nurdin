0359ffe13a619783382c78e47c89d03472029d44
hp <sahalnurdin@gmail.com>
2022-11-13 19:14:25 +0700

    changes

---
+++ Materi Pak Khannedy/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Sahal")
+	fmt.Println("Sahal Nurdin")
+	fmt.Println("Muhammad Sahal Nurdin")
+	fmt.Println(len("Muhammad Sahal Nurdin"))
+	fmt.Println("Muhammad Sahal Nurdin"[0:5])
+	fmt.Println("Muhammad Sahal Nurdin"[10])
+
+}
