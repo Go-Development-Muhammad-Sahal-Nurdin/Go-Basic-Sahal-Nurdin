64d5cefe1bce295d2a7457ce32703dc745b8051d
hp <sahalnurdin@gmail.com>
2022-11-13 19:31:12 +0700

    changes

---
+++ Materi Pak Khannedy/constant.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	const firstName string = "Muhammad Sahal Nurdin"
+	const lastName = "Nurdin"
+	const umur = 18
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
